3e4195351149aae505d8ef8ab7f47f48bb6ce9aa
LuitelSamikshya <luitelsamikshya1@gmail.com>
2022-08-03 10:04:40 -0500

    addressed review comments

--- hostsfile/hostsfile.go
+++ hostsfile/hostsfile.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	"github.com/projectdiscovery/fileutil"
-	"github.com/projectdiscovery/stringsutil"
 )
 
 const (
@@ -46,11 +45,8 @@ func Parse(p string) (map[string][]string, error) {
 		}
 
 		// discard comment part
-		if strings.Contains(line, "#") {
-			line, err = stringsutil.Before(line, "#")
-			if err != nil {
-				return nil, err
-			}
+		if idx := strings.Index(line, "#"); idx > 0 {
+			line = line[:idx]
 		}
 		tokens := strings.Fields(line)
 		if len(tokens) > 1 {
