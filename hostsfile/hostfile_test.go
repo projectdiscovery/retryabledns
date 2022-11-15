1271e0e737c949f42929e3a526a3da4bf2ca9285
xm1k3 <xm1k3@protonmail.com>
2022-11-15 15:25:14 +0100

    changes
    - corrected windowssss with windows
    - moved all tests file under hostfile

--- hostsfile/hostfile_test.go
+++ hostsfile/hostfile_test.go
@@ -8,57 +8,57 @@ import (
 
 // Linux
 func TestLinuxParsePassed(t *testing.T) {
-	elem, err := Parse("../tests/linux_host")
+	elem, err := Parse("./tests/linux_host")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
 func TestLinuxParseTabSpacesComments(t *testing.T) {
-	elem, err := Parse("../tests/linux_host_tabs_spaces_comments")
+	elem, err := Parse("./tests/linux_host_tabs_spaces_comments")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
 func TestLinuxParseSpecialChars(t *testing.T) {
-	elem, err := Parse("../tests/linux_host_special_chars")
+	elem, err := Parse("./tests/linux_host_special_chars")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
 // Mac
 func TestMacosParsePassed(t *testing.T) {
-	elem, err := Parse("../tests/macos_host")
+	elem, err := Parse("./tests/macos_host")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
 func TestMacosParseTabSpacesComments(t *testing.T) {
-	elem, err := Parse("../tests/macos_host_tabs_spaces_comments")
+	elem, err := Parse("./tests/macos_host_tabs_spaces_comments")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
 func TestMacosParseSpecialChars(t *testing.T) {
-	elem, err := Parse("../tests/macos_host_special_chars")
+	elem, err := Parse("./tests/macos_host_special_chars")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
-// Windowssss
+// Windows
 func TestWinParsePassed(t *testing.T) {
-	elem, err := Parse("../tests/win_host")
+	elem, err := Parse("./tests/win_host")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
 func TestWindowsParseTabSpacesComments(t *testing.T) {
-	elem, err := Parse("../tests/win_host_tabs_spaces_comments")
+	elem, err := Parse("./tests/win_host_tabs_spaces_comments")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
 
 func TestWinParseSpecialChars(t *testing.T) {
-	elem, err := Parse("../tests/win_host_special_chars")
+	elem, err := Parse("./tests/win_host_special_chars")
 	require.NotNil(t, elem, "host file empty")
 	require.Nil(t, err, "an error was throwed, err var is not nil")
 }
